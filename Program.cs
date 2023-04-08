f2fd51328d634ca1d6ec133670c9f01c872c1fa4
Dmitrii <molotokservice@ya.ru>
2023-04-08 21:06:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int m,n;
+Console.Write("Введите значение M: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.Write("Введите значение N: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
